0345bd6ce864ebb14609f9e1c0e531cab9fdb5fe
hajot4 <hajot4@hajot4.nti.umea>
2016-09-23 13:59:39 +0200

    See if we can fix issues with packages

--- ATD/src/paket/EndScreen.java
+++ ATD/src/paket/EndScreen.java
@@ -2,43 +2,36 @@ package paket;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class Test extends BasicGameState {
+//screen displayed when the game is over, ie when the player has won or lost
+public class EndScreen extends BasicGameState{
 
+	Image lossScreen;
 	StateBasedGame game;
-
-	Rectangle test1;
+	
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
+		lossScreen = new Image("LossScreen.png");
 		this.game = game;
-		test1 = new Rectangle(10,0,10,10);
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-		g.draw(test1);
+		
 	}
 
-	int x = 0;
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-		this.game.enterState(1);
 		
-		test1 = new Rectangle(x,x,10,10);
-	
-		x++;
-		if(x > 799){
-			x = 0;
-		}
 	}
 
 	@Override
 	public int getID() {
-		return 0;
+		return 1;
 	}
 
 }
