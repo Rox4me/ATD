aaf6946a39ec5de2cbe07c6010e6c2176eb9f40a
adrot4 <adrot4@adrot4.nti.umea>
2016-09-26 10:36:31 +0200

    Satte basen för Tower

--- ATD/src/paket/Tower.java
+++ ATD/src/paket/Tower.java
@@ -1,5 +1,20 @@
 package paket;
 
 public class Tower {
-
+	private int maxTurrets=50;
+	private int Nturrets=0;
+	private Tower1[] turrets= new Tower1[50] ;
+	
+	public void createTower(){
+		if(Nturrets>=maxTurrets){}
+		else{
+			turrets[Nturrets]= new Tower1();
+			Nturrets++;
+		}
+	}
+	public void shoot(){
+		for(int i=0;i>=Nturrets;i++){
+			turrets[i].shootTroop();
+		}
+	}
 }
---
+++ ATD/src/paket/Tower1.java
@@ -0,0 +1,19 @@
+package paket;
+
+public class Tower1 {
+	//Rate Of Fire
+	private int ROF=1;
+	//DaMaGe
+	private int DMG=1;
+	//coordinates
+	private int x=50;
+	private int y=50;
+	
+	public Tower1(){
+		
+	}
+	public void shootTroop(){
+		
+	}
+
+}
