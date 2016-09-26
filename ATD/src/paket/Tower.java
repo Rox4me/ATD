80c2040ec5222bb3684a271e630be52db5b9402c
hajot4 <hajot4@hajot4.nti.umea>
2016-09-26 13:08:31 +0200

    Worked on towers and troops, they can now interact.

--- ATD/src/paket/Tower.java
+++ ATD/src/paket/Tower.java
@@ -1,20 +1,25 @@
 package paket;
 
 public class Tower {
-	private int maxTurrets=50;
-	private int Nturrets=0;
-	private Tower1[] turrets= new Tower1[50] ;
+	private static int maxTurrets=50;
+	public static int Nturrets=0;
+	//array of all towers on the map
+	public static Tower1[] turrets= new Tower1[50] ;
 	
-	public void createTower(){
+	public static void createTower(){
+		//only create turrets if the max limit has not been reached
 		if(Nturrets>=maxTurrets){}
 		else{
+			//create new turret
 			turrets[Nturrets]= new Tower1();
 			Nturrets++;
 		}
 	}
+/*
 	public void shoot(){
 		for(int i=0;i>=Nturrets;i++){
 			turrets[i].shootTroop();
 		}
 	}
+	*/
 }
