87c1310f18f09c6f6faf2b0916eecb4029069bc5
hajot4 <hajot4@hajot4.nti.umea>
2016-09-27 10:50:42 +0200

    New endscreens, removed old ones.

--- ATD/src/paket/EndScreen.java
+++ ATD/src/paket/EndScreen.java
@@ -3,6 +3,7 @@ package paket;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -17,8 +18,8 @@ public class EndScreen extends BasicGameState{
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
-		lossScreen = new Image("textures/LossScreen.png");
-		winScreen = new Image("textures/WinScreen.png");
+		lossScreen = new Image("textures/gameover.png");
+		winScreen = new Image("textures/winner.png");
 		this.game = game;
 		
 		if(Enemy.getHealth() <= 0){
@@ -41,7 +42,11 @@ public class EndScreen extends BasicGameState{
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-		
+		//exit game
+		if(container.getInput().isKeyDown(Input.KEY_ESCAPE)){
+			container.exit();
+		}
+
 	}
 
 	@Override
